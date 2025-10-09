65a23d5d54ecc3b79329d4ec7b11517c3e34f384
C Crawford <chris@ccrawford.org>
2025-10-09 17:26:54 -0500

    Added a second control. Code is ugly AF but it's working fine.

--- CC_G5_Slave/CC_G5_Slave/src/main.cpp
+++ CC_G5_Slave/CC_G5_Slave/src/main.cpp
@@ -6,14 +6,28 @@
 #define ENCODER_PIN_A 2     // Rotary encoder pin A
 #define ENCODER_PIN_B 3     // Rotary encoder pin B
 #define ENCODER_PIN_BTN 4   // Encoder built-in button
-#define EXTRA_BUTTON 5      // Second separate button
-#define LED_PIN 6           // LED pin
+#define EXTRA_BUTTON 16      // Second separate button
+#define LED_PIN 17           // LED pin
 #define I2C_SDA_PIN 8       // I2C SDA pin
 #define I2C_SCL_PIN 9       // I2C SCL pin
 #define INT_PIN 10          // Interrupt pin to ESP32 (connects to GPIO16 on ESP32)
+
+// Channel 2
+#define ENCODER2_PIN_A 18     // Rotary encoder pin A
+#define ENCODER2_PIN_B 19     // Rotary encoder pin B
+#define ENCODER2_PIN_BTN 20   // Encoder built-in button
+#define EXTRA2_BUTTON 21      // Second separate button
+#define LED2_PIN 22           // LED pin
+#define I2C2_SDA_PIN 6       // I2C SDA pin
+#define I2C2_SCL_PIN 7       // I2C SCL pin
+#define INT2_PIN 5          // Interrupt pin to ESP32 (connects to GPIO16 on ESP32)
+
+
 #define I2C_SLAVE_ADDR 0x08 // I2C slave address
+#define I2C2_SLAVE_ADDR 0x08 // I2C slave address
 
 RotaryEncoder encoder(ENCODER_PIN_A, ENCODER_PIN_B, RotaryEncoder::LatchMode::FOUR3);
+RotaryEncoder encoder2(ENCODER2_PIN_A, ENCODER2_PIN_B, RotaryEncoder::LatchMode::FOUR3);
 
 void request_event();
 void receive_event(int byte_count);
@@ -27,6 +41,21 @@ bool encoder_button = false;
 bool extra_button = false;
 
 unsigned long last_transmission = 0;
+
+
+void request2_event();
+void receive2_event(int byte_count);
+void signal2_data_change();  // Tell the esp that it needs to fetch data. 
+void button2_interrupt();
+
+int encoder2_delta = 0;
+bool data2_changed = false;
+bool led2_state = false;
+bool encoder2_button = false;
+bool extra2_button = false;
+
+unsigned long last2_transmission = 0;
+
 
 
 
@@ -53,28 +82,77 @@ void setup()
   Wire.onReceive(receive_event);
 
   Serial.println("G5 i2c slave");
+
+  // Setup channel 2
+  
+  pinMode(INT2_PIN, OUTPUT);
+  digitalWrite(INT2_PIN, HIGH);
+
+  pinMode(EXTRA2_BUTTON, INPUT_PULLUP);
+  pinMode(ENCODER2_PIN_BTN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(EXTRA2_BUTTON), button2_interrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER2_PIN_BTN), button2_interrupt, CHANGE);
+
+
+  Wire1.setSDA(I2C2_SDA_PIN);
+  Wire1.setSCL(I2C2_SCL_PIN);
+  Wire1.begin(I2C2_SLAVE_ADDR);
+  Wire1.onRequest(request2_event);
+  Wire1.onReceive(receive2_event);
+
 }
 
 void loop()
 {
+
+  static auto lastUpdate = millis();
   static int pos = 0;
   encoder.tick();
+
   int newPos = encoder.getPosition();
-  if (data_changed || (pos != newPos && (millis() - last_transmission) > 100))   // Only send every few milliseconds
+//   if ((data_changed || (pos != newPos)) && ((millis() - last_transmission) > 100))   // Only send every few milliseconds
+  if (data_changed || ( (pos != newPos) && (millis() - lastUpdate) > 50 ))   // Only send every few milliseconds
   {
-    Serial.printf("[%ld] Pos: %d  Dir: %d EncBtn: %d  ExtraBtn: %d\n",millis(), newPos, encoder.getDirection(), digitalRead(ENCODER_PIN_BTN), digitalRead(EXTRA_BUTTON));
-    encoder_delta = newPos - pos;
+    encoder_delta += (newPos - pos) ;
+    Serial.printf("[%ld] Pos: %d  Dir: %d EncDelta: %d EncBtn: %d  ExtraBtn: %d\n",millis(), newPos, encoder.getDirection(), encoder_delta, digitalRead(ENCODER_PIN_BTN), digitalRead(EXTRA_BUTTON));
     signal_data_change();
     pos = newPos;
-    last_transmission = millis();
+    lastUpdate = millis();
     data_changed = false;
   }
+
+  static auto last2Update = millis();
+  static int pos2 = 0;
+  encoder2.tick();
+
+  int new2Pos = encoder2.getPosition();
+//   if ((data_changed || (pos != newPos)) && ((millis() - last_transmission) > 100))   // Only send every few milliseconds
+  if (data2_changed || ( (pos2 != new2Pos) && (millis() - last2Update) > 50 ))   // Only send every few milliseconds
+  {
+    encoder2_delta += (new2Pos - pos2) ;
+    Serial.printf("[%ld] Pos2: %d  Dir: %d Enc2Delta: %d Enc2Btn: %d  Extra2Btn: %d\n",millis(), new2Pos, encoder2.getDirection(), encoder2_delta, digitalRead(ENCODER2_PIN_BTN), digitalRead(EXTRA2_BUTTON));
+    signal2_data_change();
+    pos2 = new2Pos;
+    last2Update = millis();
+    data2_changed = false;
+  }
+
+
+
+
+
 }
 
 void button_interrupt()
 {
   data_changed = true;
 }
+
+void button2_interrupt()
+{
+  data2_changed = true;
+}
+
 
  // Signal to ESP32 that data has changed via interrupt pin
  void signal_data_change() {
@@ -87,6 +165,16 @@ void button_interrupt()
      // Leave data_changed flag set until data is read by ESP32
  }
 
+  // Signal to ESP32 that data has changed via interrupt pin
+ void signal2_data_change() {
+     // Pull interrupt line LOW to signal ESP32
+     digitalWrite(INT2_PIN, LOW);
+     
+     // Short pulse (1ms) is enough for ESP32 to detect
+     delay(1);
+     digitalWrite(INT2_PIN, HIGH);
+     // Leave data_changed flag set until data is read by ESP32
+ }
 
  // Handler for I2C data requests from master
  void request_event() {
@@ -100,9 +188,23 @@ void button_interrupt()
    data_changed = false;
    last_transmission = millis();
    
- //  Serial.println("Data sent to ESP32");
  }
- 
+
+  // Handler for I2C data requests from master
+ void request2_event() {
+   // Format: [encoder_delta (1 byte), encoder_button (1 byte), extra_button (1 byte)]
+   Wire1.write(encoder2_delta);
+   Wire1.write(!digitalRead(ENCODER2_PIN_BTN));
+   Wire1.write(!digitalRead(EXTRA2_BUTTON));
+   
+   // Reset delta and data_changed flag after sending
+   encoder2_delta = 0;
+   data2_changed = false;
+   last2_transmission = millis();
+   
+ }
+
+
  // Handler for I2C commands from master
  void receive_event(int byte_count) {
    if (byte_count > 0) {
@@ -122,6 +224,29 @@ void button_interrupt()
      // Read any remaining bytes (prevent buffer issues)
      while (Wire.available()) {
        Wire.read();
+     }
+   }
+ }
+
+  // Handler for I2C commands from master
+ void receive2_event(int byte_count) {
+   if (byte_count > 0) {
+     byte command = Wire1.read();
+     
+     // Command for LED control
+     if (command == 0x01) {
+       if (byte_count > 1) {
+         byte led2_value = Wire1.read();
+         led2_state = (led2_value != 0);
+         digitalWrite(LED2_PIN, led2_state ? HIGH : LOW);
+         Serial.print("LED2 state changed to: ");
+         Serial.println(led2_state ? "ON" : "OFF");
+       }
+     }
+     
+     // Read any remaining bytes (prevent buffer issues)
+     while (Wire1.available()) {
+       Wire1.read();
      }
    }
  }
